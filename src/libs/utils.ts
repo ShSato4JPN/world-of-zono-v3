d5770f73073772959c92792bf726a1eccf75d47e
ShSato4JPN <shsato4.private.1221@gmail.com>
2024-04-10 00:44:15 +0900

    fix: article styles

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,6 +1,6 @@
 export const removeTagString = (str: string): string => {
   return str.replace(
-    /<div[^>]*>|<\/div>|<a[^>]*>|<\/a>|<p[^>]*>|<\/p>|<span[^>]*>|<\/span>|<br\s*\/?>|<img[^>]*>|\n/gi,
+    /<div[^>]*>|<\/div>|<a[^>]*>|<\/a>|<p[^>]*>|<\/p>|<span[^>]*>|<\/span>|<br\s*\/?>|<img[^>]*>|<ul[^>]*>|<\/ul>|<li[^>]*>|<\/li>|\n/gi,
     "",
   );
 };
